1501ff1559ab17067788046c873d13c3df9b9180
Kirill <kirillshapoval0109@gmail.com>
2026-06-11 19:39:09 +0300

    Finished JS and starded adaptivity

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const shiny = document.querySelector(".shiny_block");
+const sec1 = document.querySelector(".section1");
+const sec2 = document.querySelector(".section2");
+const aboutMe = document.getElementById("aboutMe");
+const exp = document.getElementById("exp");
+let expTop = exp.offsetTop;
+
+document.addEventListener("mousemove", (event) => {
+    shiny.style.left = event.clientX - 350 + "px";
+    shiny.style.top = event.clientY - 350 + "px";
+});
+
+window.onscroll = ()=>{
+    if (window.scrollY >= expTop) {
+        sec2.classList.add("active");
+        sec1.classList.remove("active");
+    }else{
+        sec1.classList.add("active");
+        sec2.classList.remove("active");
+    }
+    console.log("scroll")
+}
